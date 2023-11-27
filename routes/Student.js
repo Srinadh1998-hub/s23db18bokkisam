26f8a17811b15d89f78bd402b1680b53e43c9ecd
Srinadh1998-hub <142966311+Srinadh1998-hub@users.noreply.github.com>
2023-11-27 14:55:19 -0600

    part 3 and part 4 completed

--- routes/Student.js
+++ routes/Student.js
@@ -10,14 +10,27 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const student_controlers= require('../controllers/student');
 var router = express.Router();
+
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ res.redirect("/login");
+ }
+
+
 /* GET students */
 router.get('/', student_controlers.student_view_all_Page );
 module.exports = router;
 
-router.get('/detail', student_controlers.student_view_one_Page);
+router.get('/detail',secured, student_controlers.student_view_one_Page);
 
-router.get('/create', student_controlers.student_create_Page);
+router.get('/create',secured, student_controlers.student_create_Page);
 
-router.get('/update', student_controlers.student_update_Page);
+router.get('/update',secured, student_controlers.student_update_Page);
 
-router.get('/delete', student_controlers.student_delete_Page);
+router.get('/delete',secured, student_controlers.student_delete_Page);
