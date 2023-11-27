173baee48b3ba5e81582c794b0d769ccd73d18f7
Srinadh1998-hub <142966311+Srinadh1998-hub@users.noreply.github.com>
2023-11-27 15:07:46 -0600

    latest commit

--- models/student.js
+++ models/student.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose")
 const StudentSchema = mongoose.Schema({
-    s_NAME: String,
-    s_ID: Number,
+      s_NAME: {
+        type: String,
+        required: true,
+        minlength: 3, 
+        maxlength: 20
+      },
+    s_ID: {
+        type: Number,
+        required: true,
+        min: 0,
+        max:4444
+      },
     s_GRADE: Number
 })
 module.exports = mongoose.model("Student",StudentSchema)
